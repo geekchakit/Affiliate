4308e4e5c7d85a233dd2e52d53c8798a14f0ecd2
geekckait <chakitsharma444@gmail.com>
2024-06-02 01:11:44 +0530

    key changed

--- keys/development.keys.js
+++ keys/development.keys.js
@@ -5,6 +5,7 @@ module.exports = {
     'EMAIL_FORM': 'it.dev@eztruck.co',
     'PASSWORD_FORM': 'ezTruck@3579',
     'EMAIL_SERVICE': 'gmail',
-    'MONGODB_URI': 'mongodb+srv://root:akki909@cluster0.sm3rshd.mongodb.net/Affiliate',
+    // 'MONGODB_URI': 'mongodb+srv://root:akki909@cluster0.sm3rshd.mongodb.net/Affiliate',
+    'MONGODB_URI': 'mongodb+srv://info:BYPxfvpg6kNb74GR@affiliate.pjyvpvk.mongodb.net/Affiliate?retryWrites=true&w=majority&appName=affiliate',
     'BASEURL': 'http://localhost:7001',
 }
